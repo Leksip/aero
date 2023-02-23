9476c887f68555fda27429bd24ff40b71bf0a56d
Nikita Chicherin <nickita,rom@gmail.com>
2023-02-24 02:39:04 +0300

    изменил prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import {Environment} from './interface';
 
-export const environment: Environment= {
+export const environment: Environment = {
   production: false,
   apiKey: 'AIzaSyCHJUWaifjuHWId3b-HTFJe-TTQVj9_iZA',
   fbDbUrl:'https://workers-list-d8e9a-default-rtdb.firebaseio.com/'
